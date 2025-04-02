a68bf9134e463434c86323372fffae19239169c9
phil <uniphil@gmail.com>
2025-04-02 12:07:03 -0400

    useRef not createRef omg laksdjflkasjdf

--- app/record/inflation.tsx
+++ app/record/inflation.tsx
@@ -1,14 +1,16 @@
-import { createRef, useState, useLayoutEffect } from 'react';
+import { useRef, useState, useLayoutEffect } from 'react';
+import useSize from '@react-hook/size';
 
 export const useOverflow = () => {
-  const ref = createRef(null);
+  const ref = useRef(null);
+  const [_, height] = useSize(ref);
   const [overflows, setOverflows] = useState(true);
   const [expanded, setExpanded] = useState(false);
   useLayoutEffect(() => {
     const el = ref.current;
     if (!el) return;
     setOverflows(el.scrollHeight > el.offsetHeight);
-  }, [ref]);
+  }, [height]);
   return [ref, overflows, expanded, setExpanded];
 }
 
